6fb09e221064530c4898c230ae3023a24cf2049b
mthorry <mthorry@gmail.com>
2017-07-14 18:26:23 -0400

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,42 @@
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |language_class, values|
+    # language_class = :oo;  values = {:ruby=>{...}}
+
+    values.each do |language, info|
+      # language = :ruby;  info = {:type=>"interpreted"}
+
+      info.each do |type, spec|
+        # type = :type;  spec = "interpreted"
+
+          if new_hash[language].nil?
+            new_hash[language] = {}
+          end
+
+          new_hash[language][:style] ||= [] #creates new :style key
+          new_hash[language][:style] << language_class #adding :language_class into new style array which is located HERE in bottom level of hash.
+
+          if new_hash[language][type].nil?
+            new_hash[language][type] = spec
+          end
+
+      end
+    end
+  end
+new_hash
 end
+
+
+# new_hash = {}
+# languages.each do |language_class, values|
+#    new_hash[language_class] = values
+#   values.each do |language,type|
+#      language
+#     type.each do |type, types|
+#        type
+#        types
+#     end
+#   end
+# end
